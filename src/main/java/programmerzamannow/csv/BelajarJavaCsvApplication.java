4e179505d6090a54d65b02594ef7758f4aa6b8b0
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-04 14:31:01 +0700

    belajar java csv

---
+++ src/main/java/programmerzamannow/csv/BelajarJavaCsvApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaCsvApplication.class, args);
+	}
+
+}
