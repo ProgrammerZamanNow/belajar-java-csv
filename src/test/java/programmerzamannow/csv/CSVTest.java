4bfb67bc4f297322117d67178cb58c063ccc649d
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-04 14:48:14 +0700

    Membaca CSV

--- src/test/java/programmerzamannow/csv/CSVTest.java
+++ src/test/java/programmerzamannow/csv/CSVTest.java
@@ -1,11 +1,16 @@
 package programmerzamannow.csv;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class CSVTest {
 
@@ -20,5 +25,18 @@ public class CSVTest {
 
         String csv = writer.getBuffer().toString();
         System.out.println(csv);
+    }
+
+    @Test
+    void readCSV() throws IOException {
+        Path path = Path.of("sample.csv");
+        Reader reader = Files.newBufferedReader(path);
+
+        CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT);
+        for (CSVRecord record : parser) {
+            System.out.println("First Name : " + record.get(0));
+            System.out.println("Last Name : " + record.get(1));
+            System.out.println("Value : " + record.get(2));
+        }
     }
 }
