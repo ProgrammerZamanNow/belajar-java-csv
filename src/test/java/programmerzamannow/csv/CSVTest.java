eadcfd015c2ba1ac934a67a0c270af4321bbf7a3
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-04 14:54:29 +0700

    CSV Header

--- src/test/java/programmerzamannow/csv/CSVTest.java
+++ src/test/java/programmerzamannow/csv/CSVTest.java
@@ -39,4 +39,34 @@ public class CSVTest {
             System.out.println("Value : " + record.get(2));
         }
     }
+
+    @Test
+    void readCSVWithHeader() throws IOException {
+        Path path = Path.of("sample.csv");
+        Reader reader = Files.newBufferedReader(path);
+
+        CSVFormat format = CSVFormat.DEFAULT.builder().setHeader().build();
+        CSVParser parser = new CSVParser(reader, format);
+        for (CSVRecord record : parser) {
+            System.out.println("First Name : " + record.get("First Name"));
+            System.out.println("Last Name : " + record.get("Last Name"));
+            System.out.println("Value : " + record.get("Value"));
+        }
+    }
+
+    @Test
+    void createCSVWithHeader() throws IOException {
+        StringWriter writer = new StringWriter();
+
+        CSVFormat format = CSVFormat.DEFAULT.builder()
+                .setHeader("First Name", "Last Name", "Value")
+                .build();
+        CSVPrinter printer = new CSVPrinter(writer, format);
+        printer.printRecord("Eko", "Khannedy", 100);
+        printer.printRecord("Budi", "Nugraha", 95);
+        printer.flush();
+
+        String csv = writer.getBuffer().toString();
+        System.out.println(csv);
+    }
 }
